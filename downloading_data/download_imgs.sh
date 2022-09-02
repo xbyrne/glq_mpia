590749c2c525fc114531077a273b479c31e11289
Xander Byrne <ajnb3@cam.ac.uk>
2022-09-02 15:06:03 +0200

    Added download_imgs.sh

---
+++ downloading_data/download_imgs.sh
@@ -0,0 +1,8 @@
+# Downloads all files in img_url_list.txt to ./img_files.
+# -nv: Non-verbal, not much output
+# -nc: no clobber, doesn't overwrite any files that are already downloaded
+# -b: background. Switch off for more info about how it's going
+# -i: draw URLs from the following text file
+# -P: send URLs to the following folder
+
+wget -nv -nc -b -i img_url_list.txt -P ./img_files
