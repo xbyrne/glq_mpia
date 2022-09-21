ca03bfd03e0c4e95ba7e1fe2fbe4b556e671f3f5
Xander Byrne (on cluster) <byrne@mpia.de>
2022-09-21 15:16:53 +0200

    Adding output spectra

--- sed_fitting/lephare/run_lephare.sh
+++ sed_fitting/lephare/run_lephare.sh
@@ -14,5 +14,6 @@ $LEPHAREDIR/source/mag_gal -t Q -c zphot.para -EB_V 0
 $LEPHAREDIR/source/mag_gal -t G -c zphot.para -MOD_EXTINC 4,8 -LIB_ASCII YES -EM_LINES YES
 # Running photo-z code?
 $LEPHAREDIR/source/zphota -c zphot.para
-# Moving output spectra to the output_spectra folder
+# Moving output spectra to the output_spectra folder, and then tarring
 mv *.spec output_spectra
+tar -czf output_spectra.tar.gz output_spectra
