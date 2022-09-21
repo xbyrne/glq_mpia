930b60c7e11456c0c7e0b80ffc4e6ebf895bea00
Xander Byrne (on cluster) <byrne@mpia.de>
2022-09-21 14:19:42 +0200

    Edits to lephare input files, inc. storing spectra

--- sed_fitting/lephare/run_lephare.sh
+++ sed_fitting/lephare/run_lephare.sh
@@ -1,7 +1,7 @@
 # Run all the LePHARE code, with my specifications
 #
 # Setting a directory name - change for your own if the repo is in a different dir
-export GLQMPIADIR='/data/beegfs/astro-storage/groups/walter/byrne'
+export GLQMPIADIR='/data/beegfs/astro-storage/groups/walter/byrne/glq_mpia'
 # Assembling libraries
 $LEPHAREDIR/source/sedtolib -t S -c zphot.para
 $LEPHAREDIR/source/sedtolib -t Q -c zphot.para
@@ -14,3 +14,5 @@ $LEPHAREDIR/source/mag_gal -t Q -c zphot.para -EB_V 0
 $LEPHAREDIR/source/mag_gal -t G -c zphot.para -MOD_EXTINC 4,8 -LIB_ASCII YES -EM_LINES YES
 # Running photo-z code?
 $LEPHAREDIR/source/zphota -c zphot.para
+# Moving output spectra to the output_spectra folder
+mv *.spec output_spectra
