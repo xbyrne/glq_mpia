ff5ade81c822296f281415e05d1cbb7985aa9836
Alexander Byrne <ajnb3@cam.ac.uk>
2023-08-28 18:48:25 +0100

    Improving progress bar

--- compile_images.py
+++ compile_images.py
@@ -4,6 +4,7 @@ Downloads and compiles the images from data/external/img_url_list.txt
 """
 import os
 import numpy as np
+from tqdm import tqdm
 import myutils
 
 BAND_ORDER = {"g": 0, "r": 1, "i": 2, "z": 3, "Y": 4}
@@ -19,7 +20,7 @@ imgs = np.zeros((num_objs, 28, 28, 5))
 
 DOWNLOADED_FILENAME = "./data/external/wget_file"
 
-for i, object_url_string in tqdm(enumerate(urls_list_by_object)):
+for i, object_url_string in tqdm(enumerate(urls_list_by_object), total=len(urls_list_by_object)):
     FAILED_OBJECT_FLAG = False  # Innocent until proven guilty
 
     lines = object_url_string.split()  # ID & list of URLs for a particular object
@@ -30,7 +31,7 @@ for i, object_url_string in tqdm(enumerate(urls_list_by_object)):
         band = myutils.band_from_url(url)  # g/r/i/z/Y
 
         os.system(
-            f"wget -nv -O {DOWNLOADED_FILENAME} '{url}' > /dev/null"
+            f"wget -q -O {DOWNLOADED_FILENAME} '{url}' > /dev/null"
         )  # Downloads file TODO: Could go wrong?
         raw_img = myutils.fetch_image(
             DOWNLOADED_FILENAME
