6b3a7807a27c8f8a0f77e9a8a6c4a81f934a012d
Xander Byrne <ajnb3@cam.ac.uk>
2023-09-08 11:59:08 +0100

    Taking out the trash

--- downloading_data/download_imgs.sh
+++
@@ -1,8 +0,0 @@
-# Downloads all files in img_url_list.txt to ./img_files.
-# -nv: Non-verbal, not much output
-# -nc: no clobber, doesn't overwrite any files that are already downloaded
-# -b: background. Switch off for more info about how it's going
-# -i: draw URLs from the following text file
-# -P: send image files to the following folder
-
-wget -nv -nc -i ./data/external/img_url_list.txt -P ./data/external/img_files
